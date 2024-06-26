cf438cc859c924aa44157c31586ca58f4a5ef872
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2024-06-26 10:08:02 +0500

    create-edit-todo-code

--- src/Component/SingleTodo.tsx
+++ src/Component/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import todos from "../model";
 import { MdEditDocument, MdDelete } from "react-icons/md";
 import { FaCheck } from "react-icons/fa";
@@ -12,13 +12,17 @@ type singletodo = {
 const SingleTodo = ({ todo, Todos, setTodos }: singletodo) => {
   const [Edit, setEdit] = useState<Boolean>(false);
   const [EditTodo, setEditTodo] = useState<string>(todo.todo);
-  const edithandler = (id: number) => {
-    if (!Edit && !todo.iscompleated) {
-      setEdit(!Edit);
-      console.log(Edit);
-    }
-    console.log(Edit);
+  const input = useRef<HTMLInputElement>(null)
+  useEffect(()=>{
+    input.current?.focus()
+  },[Edit])
+  const edithandler = ( e:React.FormEvent,id: number) => {
+    e.preventDefault()
+    Todos.map((todo)=>(todo.id===id ? {...todo,todo:EditTodo}:todo))
+    setEdit(false)
   };
+  console.log(EditTodo);
+
   const deletetodo = (id: number) => {
     setTodos(Todos.filter((todo) => todo.id !== id));
   };
@@ -35,14 +39,17 @@ const SingleTodo = ({ todo, Todos, setTodos }: singletodo) => {
   return (
     <div >
       {Edit ? (
-       <form onSubmit={(e)=>{
-        e.preventDefault()
-       }} className="todo m-5 p-5">
-      <input type="text" value={EditTodo} onChange={(e)=>{setEditTodo(e.target.value)}} className="text-black" />
-       <div className="text-xl flex flex-row">
+       <form onSubmit={(e)=>{ edithandler(e,todo.id)}} className="todo m-5 p-5">
+      <input type="text"  ref={input} value={EditTodo} onChange={(e)=>{setEditTodo(e.target.value)}} className="text-black inp" />
+      {/* <button className="opacity-0" type="submit"></button> */}
+       <div className="text-xl flex flex-row ">
          <div
            onClick={() => {
-             edithandler(todo.id);
+            if (!Edit && !todo.iscompleated) {
+              setEdit(!Edit);
+              console.log(Edit);
+            }
+            
            }}
          >
            <MdEditDocument />
@@ -67,11 +74,15 @@ const SingleTodo = ({ todo, Todos, setTodos }: singletodo) => {
      </form>
       ) : todo.iscompleated ? (
         <div className="conpletetodo m-5 p-5">
-          <h1 className="line-through">{todo.todo}</h1>
+          <h1 className="line-through">{EditTodo}</h1>
           <div className="text-xl flex flex-row">
             <div
               onClick={() => {
-                edithandler(todo.id);
+                if (!Edit && !todo.iscompleated) {
+                  setEdit(!Edit);
+                  console.log(Edit);
+                }
+                
               }}
             >
               <MdEditDocument />
@@ -96,11 +107,14 @@ const SingleTodo = ({ todo, Todos, setTodos }: singletodo) => {
         </div>
       ) : (
         <div className="todo m-5 p-5">
-          <h1>{todo.todo}</h1>
+          <h1>{EditTodo}</h1>
           <div className="text-xl flex flex-row">
             <div
               onClick={() => {
-                edithandler(todo.id);
+                if (!Edit && !todo.iscompleated) {
+                  setEdit(!Edit);
+                  console.log(Edit);
+                }
               }}
             >
               <MdEditDocument />
